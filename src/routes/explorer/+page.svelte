758ff302608be002e7e3ed484a844a201639b4f7
Lovanirina R <lovanirina.ran@gmail.com>
2025-11-13 12:29:19 +0300

    feat: add basic map for explorer

---
+++ src/routes/explorer/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import FooterSection from '$lib/components/sections/FooterSection/FooterSection.svelte';
+	import HeaderSection from '$lib/components/sections/HeaderSection/HeaderSection.svelte';
+	import MapSection from '$lib/components/sections/MapSection/MapSection.svelte';
+</script>
+
+<div>
+	<HeaderSection />
+	<MapSection />
+	<FooterSection />
+</div>
