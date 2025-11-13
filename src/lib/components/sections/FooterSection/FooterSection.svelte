6f63a1a9eda6cd66aa05f0e5e2da92759df3a95e
Lovanirina R <lovanirina.ran@gmail.com>
2025-11-13 10:16:16 +0300

    feat: add footer section

---
+++ src/lib/components/sections/FooterSection/FooterSection.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Container from '$lib/components/shared/Container.svelte';
+	import Section from '$lib/components/shared/Section.svelte';
+</script>
+
+<Section class="py-2">
+	<Container class="flex items-center justify-center">
+		<div class="text-sm">
+			© Copyright 2025 par <span class="font-heading font-bold">GreenDay</span>
+		</div>
+	</Container>
+</Section>
---
+++ src/lib/components/sections/FooterSection/FooterSectionStories.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import FooterSection from './FooterSection.svelte';
+
+	const { Story } = defineMeta({
+		title: 'sections/FooterSection',
+		component: FooterSection
+	});
+</script>
+
+<Story name="Default" args={{}} />
