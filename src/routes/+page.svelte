fb31a7355228c95e899baeec0375f81bba6b7060
Lovanirina R <lovanirina.ran@gmail.com>
2025-11-13 14:19:15 +0300

    feat: add basic sections to each page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script>
+	import CtaSection from '$lib/components/sections/CtaSection/CtaSection.svelte';
+	import CultiverDemainSection from '$lib/components/sections/CultiverDemainSection/CultiverDemainSection.svelte';
 	import FooterSection from '$lib/components/sections/FooterSection/FooterSection.svelte';
 	import HeaderSection from '$lib/components/sections/HeaderSection/HeaderSection.svelte';
+	import HeroSection from '$lib/components/sections/HeroSection/HeroSection.svelte';
+	import PlanterAujourdhuiSection from '$lib/components/sections/PlanterAujourdhuiSection/PlanterAujourdhuiSection.svelte';
 </script>
 
 <div>
 	<HeaderSection />
-	Accueil
+	<HeroSection />
+	<PlanterAujourdhuiSection />
+	<CultiverDemainSection />
+	<CtaSection />
 	<FooterSection />
 </div>
--- src/routes/planter-aujourdhui/+page.svelte
+++ src/routes/planter-aujourdhui/+page.svelte
@@ -1,10 +1,15 @@
 <script>
+	import FaqSection from '$lib/components/sections/FaqSection/FaqSection.svelte';
 	import FooterSection from '$lib/components/sections/FooterSection/FooterSection.svelte';
 	import HeaderSection from '$lib/components/sections/HeaderSection/HeaderSection.svelte';
+	import PlanterAujourdhuiEtapesSection from '$lib/components/sections/PlanterAujourdhuiEtapesSection/PlanterAujourdhuiEtapesSection.svelte';
+	import PlanterAujourdhuiHeroSection from '$lib/components/sections/PlanterAujourdhuiHeroSection/PlanterAujourdhuiHeroSection.svelte';
 </script>
 
 <div>
 	<HeaderSection />
-	planter aujourd'hui
+	<PlanterAujourdhuiHeroSection />
+	<PlanterAujourdhuiEtapesSection />
+	<FaqSection />
 	<FooterSection />
 </div>
