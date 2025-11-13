084491cd845abc10e533c5afee2c1ece09061381
Lovanirina R <lovanirina.ran@gmail.com>
2025-11-13 18:14:28 +0300

    feat: add and update accordion

---
+++ src/lib/components/ui/accordion/accordion-content.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { Accordion as AccordionPrimitive } from 'bits-ui';
+	import { cn, type WithoutChild } from '$lib/utils.js';
+
+	let {
+		ref = $bindable(null),
+		class: className,
+		children,
+		...restProps
+	}: WithoutChild<AccordionPrimitive.ContentProps> = $props();
+</script>
+
+<AccordionPrimitive.Content
+	bind:ref
+	data-slot="accordion-content"
+	class="overflow-hidden border-t-4 text-lg data-[state=closed]:animate-accordion-up data-[state=open]:animate-accordion-down"
+	{...restProps}
+>
+	<div class={cn('p-4', className)}>
+		{@render children?.()}
+	</div>
+</AccordionPrimitive.Content>
---
+++ src/lib/components/ui/accordion/accordion.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Accordion as AccordionPrimitive } from 'bits-ui';
+
+	let {
+		ref = $bindable(null),
+		value = $bindable(),
+		...restProps
+	}: AccordionPrimitive.RootProps = $props();
+</script>
+
+<AccordionPrimitive.Root
+	bind:ref
+	bind:value={value as never}
+	data-slot="accordion"
+	class="w-full overflow-hidden rounded-lg border-4 shadow-[4px_4px_0px_0px_rgba(0,0,0,1)]"
+	{...restProps}
+/>
