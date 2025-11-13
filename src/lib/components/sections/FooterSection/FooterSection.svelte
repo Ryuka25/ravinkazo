eae63ad67527b096f20935ae821a28ed4a9855b7
Lovanirina R <lovanirina.ran@gmail.com>
2025-11-13 12:24:41 +0300

    feat: add footer section

--- src/lib/components/sections/FooterSection/FooterSection.svelte
+++ src/lib/components/sections/FooterSection/FooterSection.svelte
@@ -3,7 +3,7 @@
 	import Section from '$lib/components/shared/Section.svelte';
 </script>
 
-<Section class="py-2">
+<Section class="py-2 pt-8">
 	<Container class="flex items-center justify-center">
 		<div class="text-sm">
 			© Copyright 2025 par <span class="font-heading font-bold">GreenDay</span>
