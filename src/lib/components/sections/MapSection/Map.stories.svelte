21d93219a33dc9d7a15f3e3a6bad5b93e2c0f743
Lovanirina R <lovanirina.ran@gmail.com>
2025-11-13 10:23:05 +0300

    feat: add map components

---
+++ src/lib/components/sections/MapSection/Map.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Map from './Map.svelte';
+
+	const { Story } = defineMeta({
+		title: 'sections/MapSection/Map',
+		component: Map
+	});
+</script>
+
+<Story name="Default" args={{}} />
---
+++ src/lib/components/sections/MapSection/Map.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { cn } from '$lib/utils';
+	import { onMount } from 'svelte';
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	import 'maplibre-gl/dist/maplibre-gl.css';
+	import { initializeMap } from '$lib/stores/map.svelte';
+
+	type MapProps = HTMLAttributes<HTMLDivElement>;
+	let { class: className }: MapProps = $props();
+
+	// Ref
+	let ref: HTMLElement;
+
+	onMount(() => {
+		initializeMap(ref);
+	});
+</script>
+
+<div class={cn('h-full min-h-[80vh] bg-[#7db9e8]', className)} bind:this={ref}></div>
