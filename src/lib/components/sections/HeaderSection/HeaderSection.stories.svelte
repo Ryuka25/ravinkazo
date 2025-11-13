29401798bbb13705f9c24ef3da1c9f71150300d8
Lovanirina R <lovanirina.ran@gmail.com>
2025-11-13 12:06:57 +0300

    feat: add header section

---
+++ src/lib/components/sections/HeaderSection/HeaderSection.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import HeaderSection from './HeaderSection.svelte';
+
+	const { Story } = defineMeta({
+		title: 'sections/HeaderSection',
+		component: HeaderSection
+	});
+</script>
+
+<Story name="Default" args={{}} />
---
+++ src/lib/components/sections/HeaderSection/HeaderSection.svelte
@@ -0,0 +1,40 @@
+<script>
+	import AppButton from '$lib/components/shared/AppButton.svelte';
+	import Container from '$lib/components/shared/Container.svelte';
+	import Section from '$lib/components/shared/Section.svelte';
+	import * as Sheet from '$lib/components/ui/sheet';
+
+	import { Hamburger } from '@lucide/svelte';
+</script>
+
+<Section class="px-4">
+	<Container class="item-end flex items-center justify-between py-4">
+		<div class="font-heading text-3xl font-bold md:text-6xl">GreenDay</div>
+		<div class="hidden gap-4 md:flex">
+			<AppButton variant="secondary" class="capitalize">accueil</AppButton>
+			<AppButton variant="secondary" class="capitalize">explorer</AppButton>
+			<AppButton class="capitalize">participer</AppButton>
+		</div>
+		<div class="block md:hidden">
+			<Sheet.Root>
+				<Sheet.Trigger>
+					<AppButton variant="secondary" size="icon">
+						<Hamburger class="size-6" />
+					</AppButton>
+				</Sheet.Trigger>
+				<Sheet.Content class="p-4">
+					<Sheet.Header>
+						<Sheet.Title>
+							<div class="font-heading text-3xl font-bold md:text-6xl">GreenDay</div>
+						</Sheet.Title>
+					</Sheet.Header>
+					<div class="grid gap-4">
+						<AppButton variant="secondary" class="capitalize">accueil</AppButton>
+						<AppButton variant="secondary" class="capitalize">explorer</AppButton>
+						<AppButton class="capitalize">participer</AppButton>
+					</div>
+				</Sheet.Content>
+			</Sheet.Root>
+		</div>
+	</Container>
+</Section>
