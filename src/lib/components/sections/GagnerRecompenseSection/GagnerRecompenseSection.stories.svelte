36ee37b9ef2f8d183befd0514acb5a0d1e9e78d2
Lovanirina R <lovanirina.ran@gmail.com>
2025-11-14 11:13:02 +0300

    feat: update gagner recompense section

---
+++ src/lib/components/sections/GagnerRecompenseSection/GagnerRecompenseSection.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import GagnerRecompenseSection from './GagnerRecompenseSection.svelte';
+
+	const { Story } = defineMeta({
+		title: 'sections/GagnerRecompenseSection',
+		component: GagnerRecompenseSection
+	});
+</script>
+
+<Story name="Default" args={{}} />
---
+++ src/lib/components/sections/GagnerRecompenseSection/GagnerRecompenseSection.svelte
@@ -0,0 +1,32 @@
+<script>
+	import AppButton from '$lib/components/shared/AppButton.svelte';
+	import Container from '$lib/components/shared/Container.svelte';
+	import Section from '$lib/components/shared/Section.svelte';
+	import { MoveRight } from '@lucide/svelte';
+</script>
+
+<Section class="pt-32">
+	<Container>
+		<div class="flex items-center justify-center gap-16 p-16">
+			<div class="relative w-1/2">
+				<div class="overflow-hidden rounded-lg">
+					<img src="/assets/composition-19.png" alt="" />
+				</div>
+				<div class="absolute right-0 bottom-0 translate-x-1/2 translate-y-1/2">
+					<AppButton variant="secondary" href="/nos-partenaires">
+						Connaitre nos partenaires
+						<MoveRight class="size-6" />
+					</AppButton>
+				</div>
+			</div>
+			<div class="flex w-1/2 flex-col gap-8">
+				<div class="font-heading text-4xl font-bold">Gagnez des Ravinkazo</div>
+				<p class="text-lg">
+					Plantez un arbre, récoltez des Ravinkazo ! Chaque Ravinkazo (Ra-vin-ka-zu) vous ouvre les
+					portes à réductions, cadeaux et avantages exclusifs auprès de nos partenaires. Votre geste
+					pour la planète devient une récompense qui grandit avec vous.
+				</p>
+			</div>
+		</div>
+	</Container>
+</Section>
