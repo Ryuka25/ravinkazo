9afb655d1eb2b3a6ba86f0c8f7ebf1cf0c226fa0
Lovanirina R <lovanirina.ran@gmail.com>
2025-11-14 11:11:59 +0300

    feat: add cultiver demain section

--- src/lib/components/sections/CultiverDemainSection/CultiverDemainSection.svelte
+++ src/lib/components/sections/CultiverDemainSection/CultiverDemainSection.svelte
@@ -1,8 +1,31 @@
 <script>
+	import AppButton from '$lib/components/shared/AppButton.svelte';
 	import Container from '$lib/components/shared/Container.svelte';
 	import Section from '$lib/components/shared/Section.svelte';
+	import { MoveRight } from '@lucide/svelte';
 </script>
 
-<Section>
-	<Container>CultiverDemainSection</Container>
+<Section class="pt-32">
+	<Container>
+		<div class="flex items-center justify-center gap-16 p-16">
+			<div class="flex w-1/2 flex-col gap-8">
+				<div class="font-heading text-4xl font-bold">Cultiver Demain !</div>
+				<p class="text-lg">
+					Chaque graine plantée, chaque savoir partagé, est un acte de reconstruction — un geste
+					doux mais puissant pour retisser le lien avec le monde qui nous porte.
+				</p>
+			</div>
+			<div class="relative w-1/2">
+				<div class="overflow-hidden rounded-lg">
+					<img src="/assets/composition-19.png" alt="" />
+				</div>
+				<div class="absolute bottom-0 -translate-x-1/2 translate-y-1/2">
+					<AppButton variant="secondary" href="/explorer-demain">
+						Explorer le monde de demain
+						<MoveRight class="size-6" />
+					</AppButton>
+				</div>
+			</div>
+		</div>
+	</Container>
 </Section>
