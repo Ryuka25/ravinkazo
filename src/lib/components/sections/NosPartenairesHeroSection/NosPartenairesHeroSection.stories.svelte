c19d2feaf7828ea88ec4c4b73072655d10b504e8
Lovanirina R <lovanirina.ran@gmail.com>
2025-11-14 11:21:48 +0300

    feat: update nos partenaires hero section

---
+++ src/lib/components/sections/NosPartenairesHeroSection/NosPartenairesHeroSection.stories.svelte
@@ -0,0 +1,11 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import PlanterAujourdhuiHeroSection from './NosPartenairesHeroSection.svelte';
+
+	const { Story } = defineMeta({
+		title: 'sections/PlanterAujourdhuiHeroSection',
+		component: PlanterAujourdhuiHeroSection
+	});
+</script>
+
+<Story name="Default" args={{}} />
---
+++ src/lib/components/sections/NosPartenairesHeroSection/NosPartenairesHeroSection.svelte
@@ -0,0 +1,21 @@
+<script>
+	import Container from '$lib/components/shared/Container.svelte';
+	import Section from '$lib/components/shared/Section.svelte';
+</script>
+
+<Section class="pt-16">
+	<Container>
+		<div class="grid gap-16 rounded-lg bg-[#f2f2f2] py-16">
+			<div class="text-center font-heading text-5xl font-bold">
+				Nos
+				<span class="inline-block -rotate-2 border-4 bg-[#7db9e8] text-foreground shadow-neo">
+					Partenaires
+				</span>
+			</div>
+			<p class="mx-auto max-w-xl text-center text-xl">
+				Ils croient, comme nous, que les petits gestes font les grandes transformations. Grâce à
+				eux, notre communauté grandit, la nature respire et l'espoir s'enracine.
+			</p>
+		</div>
+	</Container>
+</Section>
